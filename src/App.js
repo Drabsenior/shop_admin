087f6ddb5a59ae572f5e5131e519b98dfc9b8d64
Abraham <abrahamyerer101@gmail.com>
2024-06-16 00:38:49 +0300

    setup the routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import './App.css'
-import MainDash from './components/MainDash/MainDash';
-import RightSide from './components/RigtSide/RightSide';
-import Sidebar from './components/Sidebar';
+import "./App.css";
+import MainDash from "./components/MainDash/MainDash";
+import RightSide from "./components/RigtSide/RightSide";
+import Sidebar from "./components/Sidebar";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import Dashboard from "./pages/Dashboard";
+import Orders from "./pages/Orders";
+import Customers from "./pages/Customers";
+import Products from "./pages/Products";
+import Analytics from "./pages/Analytics";
 
 function App() {
   return (
-    <div className="App">
-      <div className="AppGlass">
-        <Sidebar/>
-        <MainDash/>
-        <RightSide/>
+    <Router>
+      <div className="App">
+        <div className="AppGlass">
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<MainDash />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/customers" element={<Customers />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/analytics" element={<Analytics />} />
+          </Routes>
+          <RightSide />
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./Sidebar.css";
+import { Link } from "react-router-dom";
 import Logo from "../imgs/logo.png";
 import { UilSignOutAlt } from "@iconscout/react-unicons";
 import { SidebarData } from "../Data/Data";
@@ -9,53 +10,63 @@ import { motion } from "framer-motion";
 const Sidebar = () => {
   const [selected, setSelected] = useState(0);
 
-  const [expanded, setExpaned] = useState(true)
+  const [expanded, setExpaned] = useState(true);
 
   const sidebarVariants = {
     true: {
-      left : '0'
+      left: "0",
     },
-    false:{
-      left : '-60%'
-    }
-  }
-  console.log(window.innerWidth)
+    false: {
+      left: "-60%",
+    },
+  };
+
+  console.log(window.innerWidth);
+
   return (
     <>
-      <div className="bars" style={expanded?{left: '60%'}:{left: '5%'}} onClick={()=>setExpaned(!expanded)}>
-        <UilBars />
+      <div
+        className="bars"
+        style={expanded ? { left: "60%" } : { left: "5%" }}
+        onClick={() => setExpaned(!expanded)}
+      >
+        <UilBars />s
       </div>
-    <motion.div className='sidebar'
-    variants={sidebarVariants}
-    animate={window.innerWidth<=768?`${expanded}`:''}
-    >
-      {/* logo */}
-      <div className="logo">
-        <img src={Logo} alt="logo" />
-        <span>
-          Sh<span>o</span>ps
-        </span>
-      </div>
-
-      <div className="menu">
-        {SidebarData.map((item, index) => {
-          return (
-            <div
-              className={selected === index ? "menuItem active" : "menuItem"}
-              key={index}
-              onClick={() => setSelected(index)}
-            >
-              <item.icon />
-              <span>{item.heading}</span>
-            </div>
-          );
-        })}
-        {/* signoutIcon */}
-        <div className="menuItem">
-          <UilSignOutAlt />
+      <motion.div
+        className="sidebar"
+        variants={sidebarVariants}
+        animate={window.innerWidth <= 768 ? `${expanded}` : ""}
+      >
+        {/* logo */}
+        <div className="logo">
+          <img src={Logo} alt="logo" />
+          <span>
+            Sh<span>o</span>ps
+          </span>
         </div>
-      </div>
-    </motion.div>
+
+        <div className="menu">
+          {SidebarData.map((item, index) => {
+            return (
+              <Link to={item.path} key={index} className="link">
+                <div
+                  className={
+                    selected === index ? "menuItem active" : "menuItem"
+                  }
+                  onClick={() => setSelected(index)}
+                >
+                  <item.icon />
+                  <span>{item.heading}</span>
+                </div>
+              </Link>
+            );
+          })}
+          {/* signoutIcon */}
+          <div className="menuItem">
+            <UilSignOutAlt />
+          </div>
+        </div>
+      </motion.div>
     </>
   );
 };
